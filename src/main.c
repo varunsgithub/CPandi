a2e4064187f09e982b03672d1112d243958487d8
varunsingh <varsingh@seas.upenn.edu>
2025-07-31 10:04:40 -0400

    Started working on a scanner

--- src/main.c
+++ src/main.c
@@ -1,51 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+/*Declaring the REPL function here to help run the code from the command line*/
+static void repl() {
+  //Creates a char array of 1024 characters !
+  char line[1024];
+  
+  for (;;) {
+    //print the catface asking for line of code on each line
+    printf("(˃ᆺ˂): ");
+
+    //if you do not read anything then break out of the for loop
+    if (!fgets(line, sizeof(line), stdin)) {
+      //print a newline
+      printf("\n");
+      break;
+    }
+
+    //else keep interpreting the line !
+    interpret(line);
+  }
+}
+
+
+
+
+
 int main (int argc, const char* argv[]) {
     //Initialize a VM when the program runs
     initVM();
-    //Create a struct chunk
-    Chunk chunk;
-    //Initialize it
-    initChunk(&chunk);
+
+    //If there is no argument provided to the code then run the REPL
+    if (argc == 1) {
+      repl();
+    } else if (argc == 2) {
+      //If there is one argument then run the code from the file
+      runFile(argv[1]);
+    } else {
+      //Else syntax error -> use exit code 64 (incorrect syntax) and exit
+      fprintf(stderr, "Usage: clox [path]\n");
+      exit(64);
+    }
+
+
+
+
     
-    
-    //Test
-    //Storing the constant in the constant pool
-    int constant = addConstant(&chunk, 1.2);
-    //Write code to the main chunk with OP CODE and the Constant
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-      constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-
-
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-
-    //Write it ?
-    writeChunk(&chunk, OP_RETURN, 123);
-    
-    //Trying out the tests
-    disassembleChunk(&chunk, "test_chunk");
-    //Interpret a chunk of bytecode and lo ! the VM is here on its duty
-    interpret(&chunk);
     //Free the VM when exiting
     freeVM();
-    //free it !!
-    freeChunk(&chunk);
     return 0;
 }
