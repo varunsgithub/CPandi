edadfd6c112efd9b9e92ef245f7cc1cb2d3e3427
varunsingh <varsingh@seas.upenn.edu>
2025-08-01 16:16:56 -0400

    Updated scanner

--- src/scanner.c
+++ src/scanner.c
@@ -127,10 +127,13 @@ static void skipWhitespace() {
 }
 
 static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
-    //
+    //the function checks two things
+    //1) If the length of the token matches the keyword's length !
+    //2) If the memory block matches the remaining keyword (memcmp) !!!
     if (scanner.current - scanner.start == start + length && memcmp(scanner.start + start, rest, length)==0) {
         return type;
     }
+    //return the identifier
     return TOKEN_IDENTIFIER;
 
 }
@@ -146,12 +149,29 @@ static TokenType identifierType() {
         case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
         case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
         case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        //Since the words false, for and fun all start with s
+        //we manager this with checking the second alphabet.
+        case 'f':
+            if (scanner.current - scanner.start > 1) {
+                switch(scanner.start[1]) {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            } break;
         case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
         case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
         case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
         case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
         case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
         case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+            } break;
         case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
         case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
     }
@@ -163,8 +183,8 @@ static TokenType identifierType() {
 
 
 static Token identifier() {
-    while (isAlpha(peek()) ||  isDigit(peek())) advance();
-    return makeToken(identifierType());
+  while (isAlpha(peek()) || isDigit(peek())) advance();
+  return makeToken(identifierType());
 }
 
 static Token number() {
@@ -208,7 +228,7 @@ Token scanToken() {
     char c = advance();
 
     //if the lexeme is an alphabet then check if it is one of the identifier !!
-    if (isAlpha(c)) {return identfier();}
+    if (isAlpha(c)) {return identifier();}
 
 
     if (isDigit(c)) {
